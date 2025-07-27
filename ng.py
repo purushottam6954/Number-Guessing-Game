1897e4b6a8a1c1df165ebfc2a99b9cbf3102673e
purushottam6954 <purushottamkumar6955@gmail.com>
2025-07-27 21:51:37 +0530

    Added elif statement

--- ng.py
+++ ng.py
@@ -19,11 +19,10 @@ while True:
         if user_guess==random_number:
             print("You guessed it right!")
             break
+        elif user_guess > random_number:
+            print("You were above the number")
         else:
-            if user_guess > random_number:
-                print("You were above the number")
-            else:
-                print("You were below the number")
+            print("You were below the number")
 
     else:
         print("Please enter a number Only!")
