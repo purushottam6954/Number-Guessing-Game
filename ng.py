93192c2f2db053d0ce99261915ebc9dc8411f310
purushottam6954 <purushottamkumar6955@gmail.com>
2025-07-27 19:33:33 +0530

    number Guessing Gamne

---
+++ ng.py
@@ -0,0 +1,32 @@
+import random
+top_of_range=input("Enter a number: ")
+if top_of_range.isdigit():
+    top_of_range=int(top_of_range)
+    if top_of_range <=0:
+        print("Please type a number greater than 0 next time :)")
+        quit() 
+else:
+    print("Please type a number next time!")    
+    quit()    
+r=random.randint(0,top_of_range)
+guesses=0
+
+while True:
+    user_guess=input("Make a Guess! ")
+    if user_guess.isdigit():
+        guesses+=1
+        user_guess=int(user_guess)
+        if user_guess==r:
+            print("You guessed it right!")
+            break
+        else:
+            print("Try Again")
+            continue
+
+    else:
+        print("Please enter a number Only!")
+        continue  
+print("Congrats it took you "+str(guesses)+" guesses")
+    
+
+
